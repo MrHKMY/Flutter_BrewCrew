1d4ba031699d6285a603cb8f9cffa8ddcaabfaf0
Hakimi Ridzuan <mrhakimi88@gmail.com>
2021-01-30 23:21:47 +0800

    Starter code

---
+++ crew_brew/android/app/src/main/java/com/mindscape/crew_brew/MainActivity.java
@@ -0,0 +1,6 @@
+package com.mindscape.crew_brew;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ crew_brew/lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:crew_brew/screens/home/home.dart';
+import 'package:flutter/material.dart';
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    //return home or authenticate
+    return Home();
+  }
+}
