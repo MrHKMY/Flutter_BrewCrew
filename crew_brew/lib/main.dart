777a5822217ffbf39449bb4c345320ac61dedd53
Hakimi Ridzuan <mrhakimi88@gmail.com>
2021-02-04 22:51:00 +0800

    Stream and Provider

--- crew_brew/lib/main.dart
+++ crew_brew/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:crew_brew/models/user.dart';
 import 'package:crew_brew/screens/wrapper.dart';
+import 'package:crew_brew/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<TheUser>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
