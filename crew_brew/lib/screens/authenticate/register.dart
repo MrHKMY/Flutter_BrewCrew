ed771f7fa9f53132e81119ec324a8fc2b8737841
Hakimi Ridzuan <mrhakimi88@gmail.com>
2021-02-08 23:43:39 +0800

    Validator and Email Register

--- crew_brew/lib/screens/authenticate/register.dart
+++ crew_brew/lib/screens/authenticate/register.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class Register extends StatefulWidget {
 
   final Function passValue;
+
   Register({this.passValue});
 
   @override
@@ -11,10 +12,14 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+
   final AuthService _authService = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
 
   String email = "";
   String password = "";
+  String error = "";
 
   @override
   Widget build(BuildContext context) {
@@ -36,15 +41,19 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 20),
-              TextFormField(onChanged: (val) {
-                setState(() => email = val);
-              }),
+              TextFormField(
+                  validator: (val) => val.isEmpty ? "Enter your email" : null ,
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  }),
               SizedBox(height: 20),
               TextFormField(
                 obscureText: true,
+                validator: (val) => val.length < 6 ? "Password too short" : null ,
                 onChanged: (val) {
                   setState(() => password = val);
                 },
@@ -57,9 +66,18 @@ class _RegisterState extends State<Register> {
                     style: TextStyle(color: Colors.black),
                   ),
                   onPressed: () async {
-                    print(email);
-                    print(password);
+                    if (_formKey.currentState.validate()) {
+                      dynamic result = await _authService.registerWithEmailAndPassword(email, password);
+                      if (result == null){
+                        setState(() => error = "Please add valid email");
+                      }
+                    }
                   }),
+              SizedBox(height: 20,),
+              Text(
+                error,
+              style: TextStyle(color: Colors.red),
+              )
             ],
           ),
         ),
