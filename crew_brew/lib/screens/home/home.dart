92dbcbda8471cb39b05bc0369fe49897dd658983
Hakimi Ridzuan <mrhakimi88@gmail.com>
2021-02-17 23:01:47 +0800

    Firestore Stream

--- crew_brew/lib/screens/home/home.dart
+++ crew_brew/lib/screens/home/home.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crew_brew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:crew_brew/services/database.dart';
+import 'package:provider/provider.dart';
+import 'package:crew_brew/screens/home/brew_list.dart';
 
 class Home extends StatelessWidget {
 
@@ -7,17 +11,21 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text("Brew Crew"),
-        backgroundColor: Colors.brown[400],
-        elevation: 0,
-        actions: <Widget> [
-          FlatButton.icon(
-              onPressed: () async {await _auth.signOutUser();},
-              icon: Icon(Icons.person), label: Text("LogOut"))
-        ],
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text("Brew Crew"),
+          backgroundColor: Colors.brown[400],
+          elevation: 0,
+          actions: <Widget> [
+            FlatButton.icon(
+                onPressed: () async {await _auth.signOutUser();},
+                icon: Icon(Icons.person), label: Text("LogOut"))
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
